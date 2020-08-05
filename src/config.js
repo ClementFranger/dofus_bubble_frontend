e89b47b0ccfdb817633057fcfd5d1a0b885fc826
Minifranger <clement.franger@gmail.com>
2020-08-05 13:49:01 +0200

    fix endpoint for master env

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   apiGateway: {
     REGION: "eu-west-3",
-    URL: "https://i41bnp77o6.execute-api.eu-west-3.amazonaws.com/master"
+    URL: "https://qoh1hlwvj6.execute-api.eu-west-3.amazonaws.com/master"
   }
 };
